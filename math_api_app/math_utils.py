68db68fc53bde7f9d9dcba40b5096d722e2aa5cf
gfdreyur <gfdreyur@icloud.com>
2024-09-28 17:43:58 +0300

    style: apply black for math_api

--- math_api_app/math_utils.py
+++ math_api_app/math_utils.py
@@ -1,13 +1,16 @@
 from math import factorial as math_factorial
 
+
 def factorial(n: int) -> int:
     return math_factorial(n)
 
+
 def fibonacci(n: int) -> int:
     a, b = 0, 1
-    for _  in range(n):
+    for _ in range(n):
         a, b = b, a + b
     return a
+
 
 def mean(numbers: list) -> int:
     return sum(numbers) / len(numbers)
